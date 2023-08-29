18dbf3f12439f1603784f8109d1a762b3e5c2354
KardanovIR <kardinal3094@gmail.com>
2023-08-29 21:25:10 +0300

    Fixed wallets list

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import {createRoot} from "react-dom/client";
 import {AppearanceProvider, Section, Page, Cell, Switch} from "@twa-dev/mark42";
 import {InitialsAvatar} from '@twa-dev/mark42';
 import {useColorScheme, useTheme, usePlatform} from "@twa-dev/mark42";
-import {TonConnectButton, TonConnectUIProvider, useTonAddress} from "@tonconnect/ui-react";
+import {TonConnectButton, TonConnectUIProvider, useTonAddress, TonConnect} from "@tonconnect/ui-react";
 
 
 const MyButton = (props) => {
@@ -32,7 +32,11 @@ const Address = () => {
 
 const root = createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <TonConnectUIProvider manifestUrl="https://twa-test-beryl.vercel.app/tonconnect-manifest.json">
+  <TonConnectUIProvider
+    connector={new TonConnect({
+      manifestUrl: "https://twa-test-beryl.vercel.app/tonconnect-manifest.json",
+      walletsListSource: 'https://raw.githubusercontent.com/ton-blockchain/wallets-list/feature/at-wallet/wallets.json'
+    })}>
 
     <AppearanceProvider theme={"apple"} colorScheme={"light"}>
 
